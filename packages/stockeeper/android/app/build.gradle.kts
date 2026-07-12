824923a382848346842d5785ce397dd887ec9cc6
Fransisco Wijaya <bidipeppercrap@outlook.com>
2026-07-13 06:27:17 +0700

    chore: optimize stockeeper apk build by splitting per ABI and update android configs

--- packages/stockeeper/android/app/build.gradle.kts
+++ packages/stockeeper/android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.retale.retale_stockeeper"
         // mobile_scanner requires API 23+ (Flutter's default is 21).
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
