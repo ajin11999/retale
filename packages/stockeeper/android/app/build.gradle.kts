3be7256c1ba3bff4a274690617753b216bb949aa
Fransisco Wijaya <fransiscowijaya1999@gmail.com>
2026-07-17 11:26:12 +0700

    fix(stockeeper): update java target to 17 and minSdk to 23 for mobile_scanner 7

--- packages/stockeeper/android/app/build.gradle.kts
+++ packages/stockeeper/android/app/build.gradle.kts
@@ -22,18 +22,18 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         applicationId = "com.retale.retale_stockeeper"
         // mobile_scanner requires API 23+ (Flutter's default is 21).
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
